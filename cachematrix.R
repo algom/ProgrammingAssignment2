7afde46349ea6beaed83114581dac9dc4978e4bd
algom <alejogomez61@hotmail.com>
2014-12-22 00:11:54 +0100

    Commit of cachematrix R script to cache/generate an inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions should retrieve the inverse of a square matrix, the first 
+## one should cache the result, in order to save time for looping instances in
+## in which this calculation has to be done over an over
 
-## Write a short comment describing this function
+## The first function creates a cached vector with the inverse matrix
+#which will set/get the value of a cached vector and set/get the inverse of a
+##matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { #Creation of the vector as matrix
+    inv <- NULL #Start vector with NULL
+    set <- function(y) { 
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x #Gets the value of the inverse matrix
+    setinverse <- function(solve) inv <<- solve #Setting the inverse of the function
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse) #Tag the elements of the list with names
 }
 
 
-## Write a short comment describing this function
+## This function will look for the innverse of matrix. First will look if the 
+## inverse is already stored in a cached vector. If not, it will calculate the
+## inverse and then it will set it for future use with the setinverse function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) { #Look if the inverse already exist, if not it will calculate it
+        message("Looking for cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...) #Function to inverse a matrix
+    x$setinverse(inv) #Set the inverse to store it in the cache
+    inv #Prints the inverse of the matrix
 }
+
